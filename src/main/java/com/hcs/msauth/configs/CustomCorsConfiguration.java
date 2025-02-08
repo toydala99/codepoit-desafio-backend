4e599b4b026d2eefc08bb2138fd4bce6b6272ca0
toydala99 <antoniodala83@gmail.com>
2025-02-08 19:36:15 +0100

    CrossOrigin

--- src/main/java/com/hcs/msauth/configs/CustomCorsConfiguration.java
+++ src/main/java/com/hcs/msauth/configs/CustomCorsConfiguration.java
@@ -1,18 +1,20 @@
 package com.hcs.msauth.configs;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.List;
 
+@Primary
 @Component
 public class CustomCorsConfiguration implements CorsConfigurationSource {
     @Override
     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:8080", "http://127.0.0.1:8080"));
+        config.setAllowedOrigins(List.of("http://localhost:8080", "http://127.0.0.1:8080", "*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         return config;
